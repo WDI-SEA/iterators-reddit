6fa40e75612d2d5c6f9598362a6e6cb904b53bb3
JonathanAndrew <jonandrewstevenson@yahoo.com>
2016-01-25 17:15:50 -0800

    All for questions answered

--- reddit.js
+++ reddit.js
@@ -1,5 +1,37 @@
 var reddit = require("./data.json")
 
 // Write your solutions below
+// Question 1
+var titles = reddit.data.children.forEach(function(titleName){
+	console.log(titleName.data.title);
+});
+// Question 2
+var links = reddit.data.children.map(function(link){
+	return 'http://reddit.com' + link.data.permalink;
+})
 
-console.log(reddit["data"]);
+console.log(links);
+
+// Question 3
+var hasText = function(child){
+	return child.data.selftext.length > 0;
+};
+
+var check = console.log(reddit.data.children.filter(hasText));
+
+console.log(check);
+
+// Question 4
+
+var add = function(a,b){
+	return a + b.data.score;
+};
+
+var sum = reddit.data.children.reduce(add, 0);
+
+console.log(sum);
+
+
+
+
+// console.log(reddit["data"]);
