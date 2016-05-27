9298fc06b07202cf17bb46e5d42fc6d4fece88f1
Kemal Deger <kedeger@uw.edu>
2016-05-27 08:02:09 -0700

    Reddit deliverable (Kemal)

--- src/js/reddit.js
+++ src/js/reddit.js
@@ -1,5 +1,36 @@
 var reddit = require('../data.json');
 
-// Write your solutions below
+// Answer for #1
+/*
+reddit.data.children.forEach(function (element, idx) {
+  console.log(element.data.title);
+});
+*/
 
-console.log(reddit.data);
+// Answer for #2
+/*
+var myArray = [];
+var titles = reddit.data.children.map(function (element, idx) {
+  myArray.push("http://reddit.com" + element.data.permalink);
+});
+
+console.log(myArray)
+*/
+
+/*
+// Answer for #3
+var noSelftext = reddit.data.children.filter(function(element, idx) {
+  return(reddit.data.children[idx].data.selftext);
+});
+console.log(noSelftext);
+*/
+
+
+// Answer for #4
+var runningTotal = 0;
+
+var forReduceScore = reddit.data.children.reduce(function(count, element) {
+ return count + (element.data.score);
+}, runningTotal);
+
+console.log(forReduceScore);
