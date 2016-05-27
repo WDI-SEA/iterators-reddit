5746a2002347d9b802f85077dff726617d940b56
Ivar Lee <ivarr5@gmail.com>
2016-05-26 19:03:11 -0700

    pulled data from a reddit json file using FOREACH  MAP FILTER AND REDUCE

--- src/js/reddit.js
+++ src/js/reddit.js
@@ -2,4 +2,41 @@ var reddit = require('../data.json');
 
 // Write your solutions below
 
-console.log(reddit.data);
+// use forEach to get post titles
+
+var forEachResult = reddit.data.children.forEach(function(element) {
+  console.log(element.data.title + '\n');
+});
+
+console.log('---------------------------------------------------------');
+
+// map - use map to return a new array of CHANGED elements
+
+var mapResult = reddit.data.children.map(function(element) {
+  return 'http://reddit.com' + element.data.permalink;
+});
+
+console.log('---------------------------------------------------------');
+
+// filter - use filter to return an array then run a for each loop over that
+
+var filterResult = reddit.data.children.filter(function(element) {
+  return (element.data.selftext.length > 0);
+});
+// console.log(filterResult);
+var forEachResultofFilterResult = filterResult.forEach(function(element) {
+  console.log(element.data.title + '\n');
+});
+
+console.log('---------------------------------------------------------');
+
+// reduce - Use reduce to obtain the sum of all the score values across all posts in the dataset
+var count = 0;
+var sum = reddit.data.children.reduce(function(total, elementToAddToCount) {
+  // returns the running total plus the next element
+  return total + elementToAddToCount.data.score;
+}, count);
+
+console.log(sum);
+
+console.log('---------------------------------------------------------');
