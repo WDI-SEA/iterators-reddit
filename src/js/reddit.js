1799b6776d4fb73ee5198a576b3c1b9b79d5c9ed
Daniel Rasmussen <dan.r.rasmussen@gmail.com>
2016-05-26 18:25:15 -0700

    google shopping functions like running score

--- src/js/reddit.js
+++ src/js/reddit.js
@@ -1,5 +1,50 @@
+/// EXERCISE 1
+
 var reddit = require('../data.json');
 
 // Write your solutions below
 
-console.log(reddit.data);
+// var allTitles = reddit.data.children.forEach(function(element, idx) {
+//   console.log(reddit.data.children[idx].data.title);
+// });
+
+
+
+
+// console.log(allTitles);
+
+
+///// EXERCISE 2
+
+
+// var permalinks = reddit.data.children.map(function(element, idx){
+//     return ('http://reddit.com' + reddit.data.children[idx].data.permalink);
+
+
+// })
+//  console.log(permalinks);
+
+
+//EXERCISE 3
+
+// var filt = reddit.data.children.filter(function(element, idx) {
+//           return (element.data.selftext);
+
+// })
+
+// console.log(filt);
+
+
+//EXERCISE 4
+runningTotal = 0;
+var sum = reddit.data.children.reduce(function(a, b) {
+
+
+          return a + (b.data.score)
+}, runningTotal)
+
+console.log(sum);
+
+
+
+
