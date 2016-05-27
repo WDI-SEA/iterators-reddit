250124f8aac3d56b2e98f2d4badb91a82976c7a1
darkenvy <darkenvy6@gmail.com>
2016-05-26 18:26:08 -0700

    linting

--- src/js/reddit.js
+++ src/js/reddit.js
@@ -5,37 +5,37 @@ var reddit = require('../data.json');
 // Write your solutions below
 
 // console.log(reddit.data);
-// var titles = reddit.data.children.forEach(function(element){
+// var titles = reddit.data.children.forEach(function(element) {
 //   console.log(element.data.title);
-// })
+// });
+
+// console.log(titles);
 
 
 // ================= Two =================== //
 
-// var permalinks = reddit.data.children.map(function(element){
+// var permalinks = reddit.data.children.map(function(element) {
 //   return 'http://reddit.com' + element.data.permalink;
-// })
+// });
 
 // console.log(permalinks);
 
 // ================= Three =================== //
 
-// var texts = reddit.data.children.filter(function(element, idx){
-//   if (element.data.selftext != ""){
-//     // console.log(element.data.selftext);
+// var texts = reddit.data.children.filter(function(element) {
+//   if (element.data.selftext !== '') {
 //     return true;
 //   }
-//   // return (element.data.selftext == "");
-// })
+// });
 
 // console.log(texts[0].data.selftext);
 // console.log(reddit.data.children[0].data.selftext);
 
 // ================= Four =================== //
 
-var sum = reddit.data.children.reduce(function(a, b){
+var sum = reddit.data.children.reduce(function(a, b) {
   return a + (b.data.score);
-},0)
+}, 0);
 
 
 console.log(sum);
