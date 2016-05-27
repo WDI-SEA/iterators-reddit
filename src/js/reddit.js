3b743186ce7acf0e3594c90337e7b91c8c19b3ae
Thomas <evans_760@yahoo.com>
2016-05-26 18:44:48 -0700

    .

--- src/js/reddit.js
+++ src/js/reddit.js
@@ -2,4 +2,47 @@ var reddit = require('../data.json');
 
 // Write your solutions below
 
-console.log(reddit.data);
+
+/*
+// 1)  forEach - console.log all post titles
+
+var titles = reddit.data.children.forEach(function(element, idx) {
+
+console.log(element.data.title);
+});
+
+
+// 2) map - Print an array of permalinks, append http://reddit.com to
+//    each so that they contain full http:// path to reddit
+var myArray = [];
+var plink = reddit.data.children.map(function(element, idx) {
+  myArray.push("http://reddit.com" + element.data.permalink)
+
+});
+console.log(myArray)
+
+
+//filter - Filter the posts that contain actual text in the selftext key
+
+
+
+var myArray = reddit.data.children.filter(function(element, idx) {
+  if (element.data.selftext != "") {
+    return true;
+  }
+
+
+});
+console.log(myArray)
+*/
+
+// 4) reduce - Use reduce to obtain the sum of all
+//    the score values across all posts in the dataset
+
+var initialcount = 0;
+var sum = reddit.data.children.reduce(function(element, idx) {
+  return element + (idx.data.score);
+}, initialcount);
+
+console.log(sum);
+
