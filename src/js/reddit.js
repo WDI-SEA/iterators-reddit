f05274f474f02d881900fd6b06cedc8c23957695
darkenvy <darkenvy6@gmail.com>
2016-05-26 18:09:44 -0700

    All 4

--- src/js/reddit.js
+++ src/js/reddit.js
@@ -1,5 +1,41 @@
+// ================= One =================== //
+
 var reddit = require('../data.json');
 
 // Write your solutions below
 
-console.log(reddit.data);
+// console.log(reddit.data);
+// var titles = reddit.data.children.forEach(function(element){
+//   console.log(element.data.title);
+// })
+
+
+// ================= Two =================== //
+
+// var permalinks = reddit.data.children.map(function(element){
+//   return 'http://reddit.com' + element.data.permalink;
+// })
+
+// console.log(permalinks);
+
+// ================= Three =================== //
+
+// var texts = reddit.data.children.filter(function(element, idx){
+//   if (element.data.selftext != ""){
+//     // console.log(element.data.selftext);
+//     return true;
+//   }
+//   // return (element.data.selftext == "");
+// })
+
+// console.log(texts[0].data.selftext);
+// console.log(reddit.data.children[0].data.selftext);
+
+// ================= Four =================== //
+
+var sum = reddit.data.children.reduce(function(a, b){
+  return a + (b.data.score);
+},0)
+
+
+console.log(sum);
