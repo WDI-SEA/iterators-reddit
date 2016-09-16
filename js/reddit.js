9c978aaa9e50cf213e6daa0977b2f09fdbfd2fd3
tannerkovach <kovach.tanner@gmail.com>
2016-09-15 18:13:50 -0700

    tanner kovach - parts 1 - 4

--- js/reddit.js
+++ js/reddit.js
@@ -1,9 +1,66 @@
 var reddit = require('../data.json');
 
-// Write your solutions below
+// // Write your solutions below
 
 var sum = reddit.data.children.reduce(function(total, post, idx) {
   return total + post.data.score;
 });
 
-console.log(sum);
+
+
+
+
+
+reddit.data.children.forEach(function(postTitle) {
+	console.log(reddit.data.children[0].data.title)
+});
+
+
+
+// 1.)
+
+reddit.data.children.forEach(function(postTitle) {
+	console.log(postTitle.data.title);
+});
+
+
+// 2.)
+
+reddit.data.children.map(function(permlink) {
+	console.log("http://reddit.com" + permlink.data.permalink);
+});
+
+// // 3.)
+
+var nonEmptySelfText = [];
+var selfText = [];
+ 
+ function empty(text) {
+ 	return text != "";
+ }
+ 
+ reddit.data.children.forEach(function (child){
+ 	selfText.push(child.data.selftext);
+ });
+ 
+ nonEmptySelfText = selfText.filter(empty);
+ 
+ console.log(nonEmptySelfText);
+
+// 4.)
+
+var scoreNums = reddit.data.children.map(function(answer) {
+    return (answer.data.score);
+});
+
+var add =  function(a, b){
+    return a + b;
+}
+
+var reduction = scoreNums.reduce(add);
+
+console.log(reduction);
+
+
+
+
